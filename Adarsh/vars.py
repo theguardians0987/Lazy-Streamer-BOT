b9acf36e02dc28fd43e3573b27a4c362cef06ad2
Priya <128438275+theguardians0987@users.noreply.github.com>
2023-08-14 17:28:23 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,10 +12,10 @@ class Var(object):
     API_ID = int(getenv('API_ID', '29390803'))
     API_HASH = str(getenv('API_HASH', '4227e730acf4483f59ad2799c2bd063f'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6268572600:AAHyaTkk8UDP_rFlUjwgyrQIhUiOJZRaKD0'))
-    name = str(getenv('name', 'filetolinkbot'))
+    name = str(getenv('name', 'Filestreamsbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001960380611'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '10.9.0.2'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
